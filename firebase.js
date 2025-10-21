b7496b68f20ed99ea61829300a9a168a0e28beb7
nachomorris <ijonmorris@gmail.com>
2025-10-20 21:09:35 -0300

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,24 +1,21 @@
 <script type="module">
-  // Import the functions you need from the SDKs you need
- import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+  // Importá desde una versión estable (11.0.0)
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-analytics.js";
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  // Configuración de tu proyecto
   const firebaseConfig = {
-    apiKey: "AIzaSyBMdSRkAjfWGOP0cnTUI2UEsbXBI3vTNIo",
+    apiKey: "AIzaSyBMdSRkAjfWGOP8cnTU2UEsbXBl3vTNIo",
     authDomain: "parking-c5830.firebaseapp.com",
     databaseURL: "https://parking-c5830-default-rtdb.firebaseio.com",
     projectId: "parking-c5830",
-    storageBucket: "parking-c5830.firebasestorage.app",
+    storageBucket: "parking-c5830.appspot.com",
     messagingSenderId: "242733687477",
-    appId: "1:242733687477:web:46ccc3ac60656e14d114fc",
-    measurementId: "G-W9WT1M7PTZ"
+    appId: "1:242733687477:web:46cc3ac60656e14d114fc",
+    measurementId: "G-W9WTM7PT7Z"
   };
 
-  // Initialize Firebase
+  // Inicializar Firebase
   const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+  console.log("Firebase OK ✅", app.name);
 </script>
