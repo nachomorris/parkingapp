276c031261fea2b0b0b7c8c64fe780c26e10dc71
nachomorris <ijonmorris@gmail.com>
2025-10-20 21:12:56 -0300

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,21 +1,19 @@
-<script type="module">
-  // Importá desde una versión estable (11.0.0)
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-analytics.js";
+// Importa desde una versión estable de Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-analytics.js";
 
-  // Configuración de tu proyecto
-  const firebaseConfig = {
-    apiKey: "AIzaSyBMdSRkAjfWGOP8cnTU2UEsbXBl3vTNIo",
-    authDomain: "parking-c5830.firebaseapp.com",
-    databaseURL: "https://parking-c5830-default-rtdb.firebaseio.com",
-    projectId: "parking-c5830",
-    storageBucket: "parking-c5830.appspot.com",
-    messagingSenderId: "242733687477",
-    appId: "1:242733687477:web:46cc3ac60656e14d114fc",
-    measurementId: "G-W9WTM7PT7Z"
-  };
+// Configuración de tu proyecto Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBMdSRkAjfWGOP8cnTU2UEsbXBl3vTNIo",
+  authDomain: "parking-c5830.firebaseapp.com",
+  databaseURL: "https://parking-c5830-default-rtdb.firebaseio.com",
+  projectId: "parking-c5830",
+  storageBucket: "parking-c5830.appspot.com",
+  messagingSenderId: "242733687477",
+  appId: "1:242733687477:web:46cc3ac60656e14d114fc",
+  measurementId: "G-W9WTM7PT7Z"
+};
 
-  // Inicializar Firebase
-  const app = initializeApp(firebaseConfig);
-  console.log("Firebase OK ✅", app.name);
-</script>
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+console.log("Firebase OK ✅", app.name);
